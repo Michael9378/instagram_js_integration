05f405e26695e1ab80670430367c09bf074c33d6
Mike Scott <michael@lucidagency.com>
2017-03-11 18:43:10 -0700

    Adding blacklist feature and checking for too many likes on a post already

--- javascript/like_recent_posts.js
+++ javascript/like_recent_posts.js
@@ -8,10 +8,18 @@
 // ==/UserScript==
 
 
-// Before starting this script...
-// sessionStorage.setItem("exploreTag", "motocross");
-// sessionStorage.setItem("likeFlag", "true");
-// sessionStorage.setItem("startTime", (new Date()).getTime() );
+// Before starting this script, run setUp(tag, blacklist)
+
+// setUp("motocross", ["factorybacking", "motoholics", "waspcam", "vintage_offroad", "motouniverse", "rottamg", "bonnieclassentertainment"]);
+function setUp( tag, blacklist ){
+	sessionStorage.setItem("exploreTag", tag);
+	sessionStorage.setItem("likeFlag", "true");
+	sessionStorage.setItem("startTime", (new Date()).getTime() );
+	sessionStorage.setItem("likeCount", 0);
+	sessionStorage.setItem("blacklist", JSON.stringify( blacklist ) );
+}
+
+
 
 // get start time and parse to number
 var runningTime = Number( sessionStorage.getItem("startTime") );
@@ -22,10 +30,12 @@ var fiveHours = 5*60*60*1000;
 // set like flag to false if we have been going for 5 or more hours on 1 session.
 if( runningTime > fiveHours ) {
 	sessionStorage.setItem("likeFlag", "false");
+	alert("Exceeded session time. Stopping.");
 }
 // check if we should keep moving
 if( sessionStorage.getItem("likeFlag") == "true" ) {
-	console.log("beginning like script");
+	console.log("Session run time: " + msToTime( runningTime ) );
+	console.log("Session like count: " + sessionStorage.getItem("likeCount"));
 	// make sure to wait at least 30 seconds before reloading page
 	// inject jquery and wait 20 seconds.
 	var jquery_cdn = document.createElement("script");
@@ -42,8 +52,8 @@ function likePost(){
 	$("._5kftd").remove();
 	setTimeout(function(){
 		// click first post to open it
-		// find unliked post
-		checkAndLike(0);
+		// find 13th post. This post has been up for a couple minutes.
+		checkAndLike(9);
 	}, 5000);
 }
 
@@ -60,22 +70,35 @@ function checkAndLike(index){
 	$("._8mlbc._vbtk2._t5r8b")[index].click();
 	// wait some more before liking this post.
 	setTimeout(function(){
+		// check for spammy post
+		if( checkforskip() ) {
+			index++;
+			checkAndLike(index);
+		}
 		// check if we liked this post already
 		if( $(".coreSpriteHeartOpen").length ) {
-			console.log("liking post");
-			// like post
-			$(".coreSpriteHeartOpen").click();
+			console.log("Liking post in 15 seconds.");
+			setTimeout(function(){
+				// like post
+				$(".coreSpriteHeartOpen").click();
+				// log the like
+				var likeCount = sessionStorage.getItem("likeCount");
+				likeCount = Number(likeCount);
+				likeCount++;
+				if( likeCount > 300 ){
+					sessionStorage.setItem("likeFlag", "false");
+					alert("Exceeded session like count. Stopping.");
+					return false;
+				}
+				sessionStorage.setItem("likeCount", likeCount);
 
-			// we have been on the page for atleast 15 seconds.
-			// we need to wait 15 seconds + random minute
-			var reloadWait = Math.random() * 60 * 1000;
-			// reloadWait is between 0 and 60 seconds
-			// add remaining 15 seconds and reload
-			reloadWait += 15*1000;
-			reloadWait = Math.ceil(reloadWait);
+				// we have been on the page for atleast 30 seconds.
+				var reloadWait = Math.random() * 60 * 1000;
+				reloadWait = Math.ceil(reloadWait);
 
-			console.log("reloading page in " + reloadWait/1000 + "seconds");
-			delayReload(reloadWait);
+				console.log("reloading page in " + reloadWait/1000 + "seconds");
+				delayReload(reloadWait);
+			}, 15*1000);
 		}
 		else {
 			index++;
@@ -90,4 +113,28 @@ function delayReload(waitTime) {
 		url = "https://www.instagram.com/explore/tags/" + tag + "/";
 		window.location = url;
 	}, waitTime);
+}
+
+function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100),
+        seconds = parseInt((duration/1000)%60),
+        minutes = parseInt((duration/(1000*60))%60),
+        hours = parseInt((duration/(1000*60*60))%24);
+
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + "hour " + minutes + "min " + seconds + "sec";
+}
+
+function checkforskip() {
+	var posterlink = $("._ook48").attr("href");
+	var blacklist = [];
+	blacklist = JSON.parse( sessionStorage.getItem("blacklist") );
+	for( var i = 0; i < blacklist.length; i++) {
+		if( posterlink.includes( blacklist[i] ) )
+			return true;
+	}
+
+
 }
