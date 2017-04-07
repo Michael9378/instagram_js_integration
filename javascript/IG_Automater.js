5c92a7eca3294ad0b316a6df2525e524f416662d
Mike Scott <michael@lucidagency.com>
2017-04-07 01:34:59 -0700

    Beginning class definitions for data.

--- javascript/IG_Automater.js
+++ javascript/IG_Automater.js
@@ -13,6 +13,238 @@
 // You will need a tag list, a black list, and current username from the user
 // Advanced settings allow for adjustment of like limit, likes per hour, and likes per follow/unfollow
 
+/*
+
+Data needs
+--------------------
+
+User Data  (Local)
+	- Username
+	- Follow me
+	- I follow
+	- Follow tracking
+
+User Preferences (Local)
+	- Blacklist
+	- Predefined Unfollow List
+	- Tags
+	- Daily like limit
+	- Daily follow limit
+	- Likes per hour
+
+App Needs
+	Session Storage
+		- Flag for have user preferences
+		- Flag for daily init
+		- Flag for start/stop bot
+		- Flags for actions
+			* Liking posts
+			* Scraping users to add to predefined unfollow list
+			* Follow or unfollow
+	Local Storage
+		- Flag for user authenticated
+		- Queue of unfollow users
+			* make queue (Daily follow limit)*7 to cycle through on weekly basis
+
+*/
+
+// Class constructors. JavaScript classes are garbage, so I do my own thing here.
+
+// holds information about the user that is automatically filled.
+function newUserData(){
+
+	/*
+	User Data  (Local)
+	- Username
+	- Follow me
+	- I follow
+	- Follow tracking
+	*/
+
+	// class variables
+	userData = {};
+	userData.username = false;
+	userData.followMe = [];
+	userData.iFollow = [];
+	userData.followTrack = [];
+
+	// class functions
+
+	user.updateFollowTotals = function(){
+		// TODO
+		// navigate to user page
+		// scrape followme and ifollow lists
+		// update user info and save locally.
+	}
+
+	// gets usernames that the user follows but doesn't follow them back
+	user.getNoFollowBackUsers = function(){
+		noFollowBack = [];
+		return noFollowBack;
+	}
+
+	// gets new total for follows, dates it, and stores in followTrack array
+	user.followTrackUpdate = function(){
+		user.updateFollowTotals();
+		var obj = {};
+		var d = new Date();
+		var dateString = (d.getMonth() + 1) + ", " + d.getDate() + ", " + d.getFullYear();
+		obj.date = dateString;
+		obj.followers = this.followMe.length;
+		followTrack.push( obj );
+		return obj;
+	}
+
+	// returns follows per day stat for user
+	user.getFollowsPerDay = function(){
+		var daysRan = followTrack.length;
+		if( daysRan > 1 ){
+			var total = followTrack[daysRan - 1].followers;
+			total -= followTrack[0].followers;
+			total /= daysRan;
+			return total;
+		}
+		else
+			return false;
+	}
+	// give this empty object back
+	return userData;
+}
+
+// holds information that user provides.
+function newUserPreferences(){
+
+	/*
+	User Preferences (Local)
+	- Blacklist
+	- Predefined Unfollow List
+	- Tags
+	- Daily like limit
+	- Daily follow limit
+	- Likes per hour 
+	*/
+
+	// class variables
+	userPref.blacklist = [];
+	userPref.unfollow = [];
+	userPref.tags = [];
+	userPref.tagIndex = 0;
+	userPref.likeLimit = 500;
+	userPref.followLimit = 50;
+	userPref.likesPerHour = 100;
+
+	// get the tag to follow from
+	userPref.getTag = function( increment ){
+		// set increment to true to increment the tag before pulling current tag.
+		if( increment )
+			this.tagIndex++;
+		// get the tag from tag array
+		var tag = "";
+		var index = this.tagIndex % this.tags.length;
+		tag = this.tags[ index ];
+		// return tag
+		return tag;
+	}
+
+	// set daily like limit between 10 and 1000
+	userPref.setLikeLimit = function( newLimit ) {
+		if( newLimit > 1000 )
+			newLimit = 1000;
+		if( newLimit < 10 ) {
+			newLimit = 10;
+		}
+		userPref.likeLimit = newLimit;
+	}
+
+	// set daily follow limit between 0 and 300
+	userPref.setFollowLimit = function( newLimit ) {
+		if( newLimit > 300 )
+			newLimit = 300;
+		if( newLimit < 0 ) {
+			newLimit = 0;
+		}
+		userPref.followLimit = newLimit;
+	}
+
+	// set likes per hour to be between 1 and 200
+	userPref.setLikesPerHour = function( newLimit ) {
+		if( newLimit > 200 )
+			newLimit = 200;
+		if( newLimit < 1 ) {
+			newLimit = 1;
+		}
+		userPref.likesPerHour = newLimit;
+	}
+
+	// check if a given user is on your blacklist
+	userPref.onBlacklist = function( username ){
+		for( var i = 0; i < this.blacklist.length; i++ ) {
+			if( this.blacklist[i] == username )
+				return true;
+		}
+		return false;
+	}
+
+	return userPref;
+}
+
+// holds information necessary for app functionality.
+function newAppData(){
+
+	/*
+	App Needs
+	Session Storage
+		- Flag for have user preferences
+		- Flag for daily init
+		- Flag for start/stop bot
+		- Flags for actions
+			* Liking posts
+			* Scraping users to add to predefined unfollow list
+			* Follow or unfollow
+	Local Storage
+		- Flag for user authenticated
+		- Queue of unfollow users
+			* make queue (Daily follow limit)*7 to cycle through on weekly basis
+	*/
+
+	// class variables
+	var appd = {};
+	var session = {};
+	var local = {};
+	session.dailyInit = false;
+	session.runBot = false;
+	session.follow = true;
+	session.action = 0;
+	/*
+	SESSION ACTIONS
+	---------------
+	0: Like posts
+	1: Scrape users and store
+	2: Follow/Unfollow
+	*/
+	local.authenticated = true;
+	local.unfollowQueue = [];
+	local.unfollowLength = 50/2*7; // default user pref say 50 follow actions, half are unfollows, times 7 days
+
+	appd.session = session;
+	appd.local = local;
+
+	// functions
+
+	// check that user preferences are filled correctly
+	appd.validateUserPref = function( userPref ){
+		// check that all required preferences are in the passed user pref obj
+	}
+
+	return appd;
+}
+
+// TODO: Write functions to pass same obj in and return custom objects above.
+// TODO: Write load function that loads data from storage and casts to custom objects.
+// TODO: Write save function to save data to storage.
+
+// everything below this line: TODO
+
 // Global variables
 var username;
 var tags;
@@ -265,4 +497,16 @@ function getTag() {
 	var tags = JSON.parse( localStorage.getItem("exploreTags") );
 	var index = Number(localStorage.getItem("globalAutoLikeCount"))%tags.length;
 	return tags[index];
+}
+
+// enqueue function for array
+function enQ(arr, item){
+	arr.splice(0, 0, item);
+}
+
+// dequeue function for array
+function deQ(arr){
+	if( !arr.length )
+		return false;
+	return arr.pop();
 }
